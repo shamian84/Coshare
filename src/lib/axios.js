89f7a6d6f5f64cc9837a37c8f1e033ec395c82b5
Shami Alam <161938299+shamian84@users.noreply.github.com>
2025-12-17 15:57:14 +0530

    Fix formatting and remove unnecessary newlines

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL?.replace(/\/$/, "")}/api`, 
+const api = axios.create({  baseURL: `${import.meta.env.VITE_API_URL?.replace(/\/$/, "")}/api`, 
 });
 
 api.interceptors.request.use((config) => {
