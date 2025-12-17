5a063d175ab8703526b21fe584fa6b4ff93ffbf1
Shami Alam <161938299+shamian84@users.noreply.github.com>
2025-12-17 16:04:26 +0530

    Set default API URL in axios configuration

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({  baseURL: `${import.meta.env.VITE_API_URL?.replace(/\/$/, "")}/api`, 
+const api = axios.create({  
+  baseURL: `${import.meta.env.VITE_API_URL || 'https://coshare-backend.onrender.com'}/api`,
 });
 
 api.interceptors.request.use((config) => {
